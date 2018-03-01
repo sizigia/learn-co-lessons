5a0ee633701b8f27333300e444861c0d44f6ffbb
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2018-03-01 18:27:29 -0500

    [multivariable-functions-ds-apply] update to focus on graph cost curve

--- 3d-plot.py
+++ 3d-plot.py
@@ -19,3 +19,32 @@ ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
                 cmap='viridis', edgecolor='none')
 fig = plt.figure()
 plt.show()
+
+
+
+
+import plotly
+from plotly.graph_objs import Scatter, Layout
+from plotly.offline import init_notebook_mode, iplot
+from IPython.display import display, HTML
+
+init_notebook_mode(connected=True)
+x_values = list(range(0, 6))
+
+def linear_function(x, constant):
+    return x*constant
+one_x = list(map(lambda x_value: linear_function(constant=3, x=x_value), x_values))
+trace_1 = Scatter(x=x_values, y=one_x, name="function_1 = x")
+
+layout = Layout(title="Direction of gradient ascent", autosize=False, xaxis=dict(
+        range=[0, 6]
+    ),
+    yaxis=dict(
+        range=[0, 6]
+    )
+)
+
+plotly.offline.iplot({
+    "data": [trace_1],
+    "layout": layout
+})
